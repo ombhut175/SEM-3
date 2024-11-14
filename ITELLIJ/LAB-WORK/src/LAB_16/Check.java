9d7312ca17838e8615659bc6d1c4f197f1ed2e4d
om <codersarea12@gmail.com>
2024-11-14 14:31:11 +0530

    added check

---
+++ ITELLIJ/LAB-WORK/src/LAB_16/Check.java
@@ -0,0 +1,14 @@
+package LAB_16;
+
+import java.util.PriorityQueue;
+
+public class Check {
+	public static void main(String[] args) {
+		PriorityQueue<Integer> p1 = new PriorityQueue<>();
+		p1.add(5);
+		p1.add(6);
+		p1.clear();
+		System.out.println(p1);
+
+	}
+}
